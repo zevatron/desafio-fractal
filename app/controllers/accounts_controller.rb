526399a2677b3e7bfeb2f6d9ee76f3942c17efeb
José Ricardo <zevatron@zevatron.com.br>
2019-02-02 10:28:46 -0300

    Limitação dos resources. Aninhamento Account e ajustes no controller.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,24 +1,34 @@
 class AccountsController < ApplicationController
   before_action :set_account, only: [:show, :update, :destroy]
+  before_action :get_group
+
+  def get_group
+    @group = Group.find(params[:group_id])
+  end
 
   # GET /accounts
   def index
-    @accounts = Account.all
+    if params[:group_id].nil?
+      @accounts = Account.all
+    else
+      @accounts = @group.accounts
+    end
 
-    render json: @accounts
+    render json: @accounts, except: [:created_at, :updated_at]
   end
 
   # GET /accounts/1
   def show
-    render json: @account
+    render json: @account, except: [:created_at, :updated_at]
   end
 
   # POST /accounts
   def create
     @account = Account.new(account_params)
+    @account.group = @group
 
     if @account.save
-      render json: @account, status: :created, location: @account
+      render json: @account, status: :created, location: [@group,@account]
     else
       render json: @account.errors, status: :unprocessable_entity
     end
@@ -26,6 +36,7 @@ class AccountsController < ApplicationController
 
   # PATCH/PUT /accounts/1
   def update
+    @account.group = @group
     if @account.update(account_params)
       render json: @account
     else
@@ -46,6 +57,6 @@ class AccountsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def account_params
-      params.require(:account).permit(:athlete_id, :group_id)
+      params.require(:account).permit(:athlete_id, :group_id, :status)
     end
 end
